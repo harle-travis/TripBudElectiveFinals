b01fe8b3f41df38bcbf4787db4bb54e709a16215
harle-travis <harletravismalabuyoc@gmail.com>
2024-06-25 10:52:59 +0800

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost"; // Change this if your database server is different
-$username = "root"; // Change this to your database username
-$password = ""; // Change this to your database password
+$servername = "database2.cxykm8w2arng.ap-southeast-2.rds.amazonaws.com"; // Change this if your database server is different
+$username = "admin"; // Change this to your database username
+$password = "Jbalicos27"; // Change this to your database password
 $database = "southpick_db"; // Change this to your database name
-$root = "3307";
+
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database,$root);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
